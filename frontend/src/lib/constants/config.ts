28a9ba2fcd3d9e46a55b051e83361d096097fcfe
Louis <louisavon77@gmail.com>
2026-04-29 16:00:43 +0200

    create : new frontend

--- frontend/src/lib/constants/config.ts
+++
@@ -1,7 +0,0 @@
-export const APP_CONFIG = {
-  name: 'JobAggregator',
-  apiUrl: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8080/api',
-  pageSize: 12,
-  debounceMs: 400,
-  queryStaleTime: 60_000,
-} as const
